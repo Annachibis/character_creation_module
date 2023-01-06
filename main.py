d14fc0c64aa3231389441f00218e291673302c72
Annachibis <annachebryakova@yandex.ru>
2023-01-06 23:05:05 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def attack(char_name, char_class):
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный '
-           
+            f'{5 + randint(5, 10)}')
     if char_class == 'mage':
         return (
             f'{char_name} нанёс урон противнику равный '
